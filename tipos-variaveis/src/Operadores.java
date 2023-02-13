63b58e98f3c8047a5d9de1c0b7342b4d3801794e
leo-sobrinho <leonardosobrinho1998@outlook.com>
2023-02-13 12:29:56 -0300

    att aulas

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,77 @@
+public class Operadores {
+    public static void main(String[] args) {
+        //Aula 2
+        String nomeCompleto = "LINGUAGEM " + "JAVA";
+
+        System.out.println(nomeCompleto);
+
+        String concatenacao ="?";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+1+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = 1+"1"+1+"1";
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+1+1+1;
+
+        System.out.println(concatenacao);
+
+        concatenacao = "1"+(1+1+1);
+
+        System.out.println(concatenacao);
+    
+        //Aula 3
+        
+        int numeroa3 = 5;
+
+        numeroa3 = - numeroa3;
+
+        System.out.println(numeroa3);
+
+        numeroa3 = numeroa3 * -1;
+
+        System.out.println(numeroa3);
+
+        //Aula 4
+
+        int numeroa4 = 5;
+
+        //x repeticao
+
+        //numeroa4 = numeroa4 +1;
+
+        System.out.println(--numeroa4);
+        
+        boolean variavela4 = true;
+
+        System.out.println(!variavela4);
+
+        variavela4 = !variavela4;
+        
+        System.out.println(variavela4);
+
+        //Aula 5
+
+        int a, b;
+
+        a = 5;
+        b = 6;
+        
+        String resultadoa5 = a==b ? "Verdadeiro": "Falso";
+        
+        System.out.println(resultadoa5);
+
+        //Aula 6
+
+        
+    }
+}
