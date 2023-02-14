3c2cfb522c17f84a61b23b62ebb6181a6b8314bf
leo-sobrinho <leonardosobrinho1998@outlook.com>
2023-02-14 13:07:55 -0300

    att aula 6

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -72,6 +72,34 @@ public class Operadores {
 
         //Aula 6
 
+        String nomeUm = "Leonardo";
+        String nomeDois = new String("Leonardo");
+
+        System.out.println(nomeUm.equals(nomeDois));
+        //.equals compara o conteúdo do objeto
         
+        int numero1a6 = 1;
+        int numero2a6 = 2;
+
+        String numero1a6txt = Integer.toString(numero1a6);
+        String numero2a6txt = Integer.toString(numero2a6);
+
+        System.out.println(numero1a6txt.concat(" e ") + numero2a6txt);
+        
+        boolean simNao = numero1a6 == numero2a6;
+
+        System.out.println("numero1a6 é igual a numero2a6? " + simNao);
+        
+        simNao = numero1a6 != numero2a6;
+
+        System.out.println("numero1a6 é diferente de numero2a6? " + simNao);
+
+        simNao = numero1a6 > numero2a6;
+
+        System.out.println("numero1a6 é maior que numero2a6? " + simNao);
+
+        simNao = numero1a6 < numero2a6;
+
+        System.out.println("numero1a6 é menor que numero2a6? " + simNao);
     }
 }
